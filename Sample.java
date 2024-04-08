3259f2c986e5e15de7171850c28a99f600ca8f42
Tomas Mejia <159865949+tomasmejiag0@users.noreply.github.com>
2024-04-08 16:35:30 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -4,65 +4,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DatabaseManager {
-    private static DatabaseManager instance;
-    private Connection connection;
+public class Sample
+{
+  public static void main(String[] args)
+  {
+    // NOTE: Connection and Statement are AutoClosable.
+    //       Don't forget to close them both in order to avoid leaks.
+    try
+    (
+      // create a database connection
+      Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+      Statement statement = connection.createStatement();
+    )
+    {
+      statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-    private DatabaseManager() {
-        // Private constructor to prevent instantiation from outside
+      statement.executeUpdate("drop table if exists person");
+      statement.executeUpdate("create table person (id integer, name string)");
+      statement.executeUpdate("insert into person values(1, 'leo')");
+      statement.executeUpdate("insert into person values(2, 'yui')");
+      ResultSet rs = statement.executeQuery("select * from person");
+      while(rs.next())
+      {
+        // read the result set
+        System.out.println("name = " + rs.getString("name"));
+        System.out.println("id = " + rs.getInt("id"));
+      }
     }
-
-    public static synchronized DatabaseManager getInstance() {
-        if (instance == null) {
-            instance = new DatabaseManager();
-        }
-        return instance;
+    catch(SQLException e)
+    {
+      // if the error message is "out of memory",
+      // it probably means no database file is found
+      e.printStackTrace(System.err);
     }
-
-    public Connection getConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            // Create a new connection if it doesn't exist or is closed
-            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-        }
-        return connection;
-    }
-
-    public void closeConnection() throws SQLException {
-        if (connection != null && !connection.isClosed()) {
-            connection.close();
-        }
-    }
-}
-
-public class Sample {
-    public static void main(String[] args) {
-        try {
-            // Singleton
-            DatabaseManager dbManager = DatabaseManager.getInstance();
-
-            // Creamos la instancia que conecta
-            Connection connection = dbManager.getConnection();
-            Statement statement = connection.createStatement();
-
-            statement.setQueryTimeout(30); // set timeout to 30 sec.
-
-            statement.executeUpdate("drop table if exists person");
-            statement.executeUpdate("create table person (id integer, name string)");
-            statement.executeUpdate("insert into person values(1, 'leo')");
-            statement.executeUpdate("insert into person values(2, 'yui')");
-            ResultSet rs = statement.executeQuery("select * from person");
-            while (rs.next()) {
-                
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
-
-            
-            statement.close();
-            dbManager.closeConnection();
-        } catch (SQLException e) {
-            
-            e.printStackTrace(System.err);
-        }
-    }
+  }
 }
