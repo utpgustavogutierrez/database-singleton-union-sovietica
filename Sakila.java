70498beb9e04e794a915faeff5824934f0a181a2
Tomas Mejia <159865949+tomasmejiag0@users.noreply.github.com>
2024-04-08 18:43:12 -0500

    Update Sakila.java

--- Sakila.java
+++ Sakila.java
@@ -4,33 +4,42 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Sakila
-{
-  public static void main(String[] args)
-  {
-    // NOTE: Connection and Statement are AutoClosable.
-    //       Don't forget to close them both in order to avoid leaks.
-    try
-    (
-      // create a database connection
-      Connection connection = DriverManager.getConnection("jdbc:sqlite:./sqlite-sakila.db");
-      Statement statement = connection.createStatement();
-    )
-    {
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-      ResultSet rs = statement.executeQuery("select * from film");
-      while(rs.next())
-      {
-        // read the result set
-        System.out.println("title = " + rs.getString("title"));
-      }
+public class Sakila {
+    private static Sakila instance;
+    private Connection connection;
+
+    private Sakila() {
+        try {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:./sqlite-sakila.db");
+            connection.createStatement().executeUpdate("PRAGMA foreign_keys = ON"); // Enable foreign key support
+        } catch (SQLException e) {
+            e.printStackTrace(System.err);
+        }
     }
-    catch(SQLException e)
-    {
-      // if the error message is "out of memory",
-      // it probably means no database file is found
-      e.printStackTrace(System.err);
+
+    public static synchronized Sakila getInstance() {
+        if (instance == null) {
+            instance = new Sakila();
+        }
+        return instance;
     }
-  }
+
+    public void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace(System.err);
+            }
+        }
+    }
+
+    // You can add more methods here for executing different queries
+
+    public static void main(String[] args) {
+        Sakila sakila = Sakila.getInstance();
+        sakila.closeConnection();
+    }
 }
